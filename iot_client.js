ccb36374a355090ef065c91df6dccef206a6406e
Jun Ichikawa <jichikawa@r.recruit.co.jp>
2015-12-06 16:49:41 +0900

    first commit for Raspberry Pi + AWS Iot

---
+++ iot_client.js
@@ -0,0 +1,46 @@
+var awsIot = require('aws-iot-device-sdk');
+var sys = require('sys')
+var exec = require('child_process').exec;
+
+// Define paramerters to publish a message
+var device = awsIot.device({
+    keyPath: './certs/private.pem',
+    certPath: './certs/cert.pem',
+    caPath: './certs/rootca.crt',
+    clientId: 'pi_farm_client',
+    region: 'ap-northeast-1'
+});
+
+// Connect to Message Broker
+device.on('connect', function() {
+    console.log('Connected to Message Broker.');
+
+    // Loop every 10 sec
+    setInterval(function() {
+
+        exec("python /home/pi/pi_farm/collector.py", function (error, stdout, stderr) {
+          if (error !== null) {
+            console.log('exec error: ' + error);
+            return
+          }
+
+          datas = stdout.split(",")
+          // Compose records
+          var record = {
+              "deviceid": "pi_01",
+              "timestamp": datas[0],
+              "temp": datas[1],
+              "hum": datas[2],
+              "lx": datas[3],
+              "moi": datas[4].replace(/[\n\r]/g,"")
+          };
+
+          // Serialize record to JSON format and publish a message
+          var message = JSON.stringify(record);
+          console.log("Publish: " + message);
+          device.publish('pi_farm', message);
+        });
+
+
+    }, 10000);
+});
